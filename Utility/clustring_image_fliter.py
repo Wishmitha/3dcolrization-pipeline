1d88c4f2d346f1e3faf423a40c30dfbe5cfb5a4b
Wishmitha <wishmitha.14@cse.mrt.ac.lk>
2020-05-07 19:08:45 +0800

    Updated clustering image filter

--- Utility/clustring_image_fliter.py
+++ Utility/clustring_image_fliter.py
@@ -21,22 +21,51 @@ for img in clustered_image_list:
     clustered_img_indexes.append(int(img.split('.')[0].split('/')[-1].strip("0")))
 
 #clustered_img_indexes = np.array(clustered_img_indexes)
-print(clustered_img_indexes)
-camera = o3d.io.read_pinhole_camera_trajectory(os.path.join(origin_path, "scene/key.log"))
 
+# camera = o3d.io.read_pinhole_camera_trajectory(os.path.join(origin_path, "scene/key.log"))
+#
+#
+# extrinsic_mat_list=[]
+# for index in clustered_img_indexes:
+#     extrinsic_mat_list.append(camera.parameters[index].extrinsic)
+#
+# f = open(os.path.join(clustered_path, "scene/key.log"), "w")
+# for i in range(0,len(extrinsic_mat_list)):
+#     f.writelines(str(i)+" "+str(i)+" "+str((i+1))+'\n')
+#     for j in range(0,4):
+#         for k in range(0, 4):
+#             f.writelines(str(round(extrinsic_mat_list[i][k][j],10))+" ")
+#         f.writelines('\n')
+#
+# f.close()
 
 extrinsic_mat_list=[]
+key_file = open(os.path.join(origin_path, "scene/key.log"))
+key_lines=[]
+for line in key_file.readlines():
+    key_lines.append(line)
+
+filtered_key = open(os.path.join(clustered_path, "scene/key.log"), "w")
+
+count = 0
 for index in clustered_img_indexes:
-    extrinsic_mat_list.append(camera.parameters[index].extrinsic)
+     filtered_key.writelines(str(count) + " " + str(count) + " " + str((count + 1)) + '\n')
+     for i in range(1,5):
+         filtered_key.writelines(key_lines[5*index+i])
+     count+=1
 
-f = open(os.path.join(clustered_path, "scene/key.log"), "w")
-for i in range(0,len(extrinsic_mat_list)):
-    f.writelines(str(i)+" "+str(i)+" "+str((i+1))+'\n')
-    for j in range(0,4):
-        for k in range(0, 4):
-            f.writelines(str(round(extrinsic_mat_list[i][j][k],10))+" ")
-        f.writelines('\n')
+filtered_key.close()
 
-f.close()
+
+#
+# f = open(os.path.join(clustered_path, "scene/key.log"), "w")
+# for i in range(0,len(extrinsic_mat_list)):
+#     f.writelines(str(i)+" "+str(i)+" "+str((i+1))+'\n')
+#     for j in range(0,4):
+#         for k in range(0, 4):
+#             f.writelines(str(round(extrinsic_mat_list[i][k][j],10))+" ")
+#         f.writelines('\n')
+
+
 
 
